5a419fea1c20b1408ded0d1051608ddf9f84e023
Mehdi <mehdicontacto@gmail.com>
2023-02-15 20:47:10 +0200

    Siderbar

--- src/App.js
+++ src/App.js
@@ -12,22 +12,59 @@ import FrontPage from "./Components/FrontPage/FrontPage";
  import logo from "./media/logo.png"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import {
   faYoutube,
   faFacebook,
   faTwitter,
-  faInstagram
+  faInstagram,
+  faGithub,
+  faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
+import { useEffect, useState } from 'react';
 
 
 function App() {
+
+const [sidebar, setSidebar] = useState(false)
+
+
+const change = () =>{
+  if(sidebar === true){
+    setSidebar(false)
+  }else{
+    setSidebar(true)
+  }
+}
+
+const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+useEffect(() => {
+  const handleWindowResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  window.addEventListener('resize', handleWindowResize);
+  
+  if(windowWidth > 777){
+    setSidebar(false)
+  }
+  return () => {
+    window.removeEventListener('resize', handleWindowResize);
+  };
+  
+}, [windowWidth]);
+
+
   return (
+
+    
     
     <Router basename='/miportfolio'>
-      <div className="App" >
+    <div className="App" >
+    
         <div className='logo'>
-        <Link to="/"><img src={logo} alt="" width={60}></img></Link> 
-          
+            <Link to="/"><img src={logo} alt="" width={60}></img></Link> 
         </div>
         <div className='Bar'>
             <Link to="/">Home</Link>
@@ -36,6 +73,69 @@ function App() {
             {/* <Link to="/aboutme">About Me</Link> */}
         </div>
         <div className='socialmedia'>
+            <a href="https://github.com/MehdiZaidaneS" 
+               className="youtube social" target="_blank" rel="noopener noreferrer">
+               <FontAwesomeIcon icon={faGithub} size="2x" />
+             </a>
+             <a href="https://www.facebook.com/learnbuildteach/"
+               className="facebook social" target="_blank" rel="noopener noreferrer">
+               <FontAwesomeIcon icon={faLinkedin} size="2x" />
+             </a>
+        </div>
+        <div className='bars' >
+            <button onClick={change}>
+               <FontAwesomeIcon icon={faBars} size="2x"/>
+             </button>
+        </div>
+      </div>
+
+      { sidebar &&
+      <div className='sidebar'>
+          <div className='Bar2'>
+            <Link to="/"><button onClick={change}>Home</button></Link>
+            <Link to="/projects"><button onClick={change}>Projects</button></Link>
+            <Link to="/contact"><button onClick={change}>Contact</button></Link>
+          </div>
+          <div className='socialmedia2'>
+            <a href="https://github.com/MehdiZaidaneS" 
+               className="youtube social" target="_blank" rel="noopener noreferrer">
+               <FontAwesomeIcon icon={faGithub} size="2x" />
+             </a>
+             <a href="https://www.facebook.com/learnbuildteach/"
+               className="facebook social" target="_blank" rel="noopener noreferrer">
+               <FontAwesomeIcon icon={faLinkedin} size="2x" />
+             </a>
+          </div>
+          <div className='footer2'> 
+             <a href="https://www.youtube.com/c/jamesqquick" 
+                className="youtube social" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faYoutube} size="2x" />
+              </a>
+              <a href="https://www.facebook.com/learnbuildteach/"
+                className="facebook social" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
+              </a>
+              <a href="https://www.twitter.com/jamesqquick" className="twitter social" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+              </a>
+              <a href="https://www.instagram.com/learnbuildteach"
+                className="instagram social" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+              </a>
+            </div> 
+      </div>
+      }
+
+      { sidebar === false &&
+      <Routes>
+            <Route exact path='/' element={<FrontPage/>} />
+            <Route exact path='/projects' element={<Projects/>} />
+            {/* <Route exact path='/aboutme' element={<AboutMe/>} /> */}
+            <Route exact path='/contact' element={<ContactMe/>} /> 
+      </Routes>
+     }
+
+      <div className="footer">
          <a href="https://www.youtube.com/c/jamesqquick" 
            className="youtube social" target="_blank" rel="noopener noreferrer">
            <FontAwesomeIcon icon={faYoutube} size="2x" />
@@ -51,15 +151,8 @@ function App() {
            className="instagram social" target="_blank" rel="noopener noreferrer">
            <FontAwesomeIcon icon={faInstagram} size="2x" />
          </a>
-        </div>
-        </div>
-      <Routes>
-            <Route exact path='/' element={<FrontPage/>} />
-            <Route exact path='/projects' element={<Projects/>} />
-            {/* <Route exact path='/aboutme' element={<AboutMe/>} /> */}
-            <Route exact path='/contact' element={<ContactMe/>} />
-            
-      </Routes>
+      </div>
+       
 
     </Router>
   
