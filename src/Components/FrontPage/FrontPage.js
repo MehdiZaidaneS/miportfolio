ef61985f386e0029ba1a72495f8eb6a6d48579c1
Mehdi <mehdicontacto@gmail.com>
2023-03-09 14:09:55 +0200

    Frontpage fixed

--- src/Components/FrontPage/FrontPage.js
+++ src/Components/FrontPage/FrontPage.js
@@ -15,17 +15,16 @@ const FrontPage = () => {
         <div className='parts'>
           <div className='part1'>
                 <div className='text'>
-                 <h2>HELLO,</h2>
-                 <h1>I Am Developer</h1>
-                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                 <button>Contact Me</button>
+                   <h2>Mehdi Zaidane</h2>
+                   <h1>I'M A DEVELOPER</h1>
+                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                   <button>Contact Me</button>
                </div>
                <div className='photo'>
-                 <img src={foto} alt="" width={500}></img>
+                   <img src={foto} alt="" width={500}></img>
                </div>
           </div>
           <div className='part2'>
-            
              <a href='#info' ><FontAwesomeIcon size='2x' icon={faChevronDown}></FontAwesomeIcon></a>
           </div>
         </div>
@@ -33,11 +32,31 @@ const FrontPage = () => {
         
       </div>
       <div className='block2' id='info'>
-        <div className='aboutme'>
-            <h1>About Me</h1>
+         <h1>About Me</h1>
+         <div className='aboutMe'>
+           <img src={foto} alt="" width={400}></img>
+           <div>
+             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+             <button>Download CV</button>
+           </div>
+           
          </div>
-         <div className='recentprojects'>
-            <h1>Recent projects</h1>
+         <div className='myInfo'>
+           <div>
+             <button>Skills</button>
+             <button>Experience</button>
+             <button>Education</button>
+           </div>
+           <div>
+
+           </div>
+           <div>
+
+           </div>
+           <div>
+
+           </div>
          </div>
       </div>
      
